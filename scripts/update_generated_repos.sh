63887d30b6add83c5cae85103929821a911d6740
WTPENGUIN <ccho2932@naver.com>
2022-09-27 14:43:59 +0900

    github action setting

--- scripts/update_generated_repos.sh
+++ scripts/update_generated_repos.sh
@@ -2,8 +2,8 @@
 
 # Config for auto-building
 git remote rename origin upstream
-git config --global user.email "bot@pixhawk.org"
-git config --global user.name "PX4BuildBot"
+git config --global user.email "peach@thepeach.kr"
+git config --global user.name "ThePeach-kr"
 git config --global credential.helper "store --file=$HOME/.git-credentials"
 echo "https://${GH_TOKEN}:@github.com" > "$HOME"/.git-credentials
 
@@ -11,13 +11,13 @@ echo "https://${GH_TOKEN}:@github.com" > "$HOME"/.git-credentials
 GEN_START_PATH=$PWD
 mkdir -p include/mavlink/v2.0
 cd include/mavlink/v2.0
-git clone https://github.com/mavlink/c_library_v2.git
+git clone https://github.com/the-peach-drone/c_library_v2.git
 cd ../../..
 ./scripts/update_c_library.sh 2
 # v1.0 legacy
-cd "$GEN_START_PATH"
-mkdir -p include/mavlink/v1.0
-cd include/mavlink/v1.0
-git clone https://github.com/mavlink/c_library_v1.git
-cd ../../..
-./scripts/update_c_library.sh 1
+#cd "$GEN_START_PATH"
+#mkdir -p include/mavlink/v1.0
+#cd include/mavlink/v1.0
+#git clone https://github.com/mavlink/c_library_v1.git
+#cd ../../..
+#./scripts/update_c_library.sh 1
